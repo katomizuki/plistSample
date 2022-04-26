52d4a0c81d344144288782adb0d14fc3e14bfb61
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-04-26 14:25:47 +0900

    Initial Commit

---
+++ plistSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  plistSample
+//
+//  Created by ミズキ on 2022/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
